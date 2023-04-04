f08cc555b4337cdb592304481be359d1c6de4265
Yatish <Yatish@DESKTOP-IK14MLI>
2023-04-04 18:31:54 +0530

    second commit

---
+++ Dayone/src/in/accenture/Chardemo.java
@@ -0,0 +1,15 @@
+package in.accenture;
+
+public class Chardemo {
+	public static void main(String[]args) {
+		char ch='a';
+		System.out.println(ch);
+		//asssign number to character
+		char ch1=70;
+		System.out.println(ch1);
+		//assign unicode to character
+		char ch2='\u00A3';
+		System.out.println(ch2);
+	}
+
+}
---
+++ Dayone/src/in/accenture/PrimitiveDatatype.java
@@ -0,0 +1,18 @@
+package in.accenture;
+
+public class PrimitiveDatatype {
+	public static void main(String[]args) {
+		byte bytemax=127;
+		short shortmax=32767;
+		long var1=5464364L;
+		char var2='y';
+		boolean flag=true;
+		System.out.println(bytemax);
+		System.out.println(shortmax);
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(flag);
+		
+	}
+
+}
