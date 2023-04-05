c97d6ba72d1020666724a9c4a96a6ad5ea55c4d4
Yatish <Yatish@course>
2023-04-05 16:56:51 +0530

    DayTwo commit

---
+++ Daytwo/src/orgtnsifcompilerinputdemo/Compilerinputprogram.java
@@ -0,0 +1,13 @@
+package orgtnsifcompilerinputdemo;
+
+public class Compilerinputprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//cpmpile time input
+		float salary=67400.50f;
+		System.out.println("Salary is:"+salary);
+
+	}
+
+}
