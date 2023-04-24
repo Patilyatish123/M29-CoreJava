5bbcb6cd554847af12a74ecbfbb633ed93f0d01c
Yatish <Yatish@DESKTOP-IK14MLI>
2023-04-24 16:46:23 +0530

    first commit

---
+++ Assignment no 1/src/functionrecusrsiondemo/AddTwoNumberUsingFunction.java
@@ -0,0 +1,27 @@
+package functionrecusrsiondemo;
+import java.util.*;
+
+public class AddTwoNumberUsingFunction {
+	
+
+		public static void main(String[] args) {
+			
+			@SuppressWarnings("resource")
+			Scanner s = new Scanner(System.in);
+			
+	        System.out.print("Enter first number: ");
+	        int num1 = s.nextInt();
+	        
+	        System.out.print("Enter second number: ");
+	        int num2 = s.nextInt();
+	        
+	        int sum = add(num1, num2);
+	        System.out.println("Sum of number is " + sum);
+	    }
+
+	    public static int add(int a, int b) {
+	        return a + b;
+	    }
+	}
+
+
---
+++ Assignment no 1/src/functionrecusrsiondemo/Sumogdigit.java
@@ -0,0 +1,29 @@
+package functionrecusrsiondemo;
+import java.util.Scanner;
+
+public class Sumogdigit {
+
+		
+			public static int sum(int n)
+		    {
+		        if (n == 0)
+		            return 0;
+		        return (n % 10 + sum(n / 10));
+		    }
+		 
+		  
+		    public static void main(String args[])
+		    {
+		    	Scanner s=new Scanner(System.in);
+		    	System.out.println("Enter the number");
+		    	int num = s.nextInt();
+		    	
+		        int result = sum(num);
+		        System.out.println("Sum of digits in " + num + " is " + result);
+			}
+
+		}
+
+	
+
+
