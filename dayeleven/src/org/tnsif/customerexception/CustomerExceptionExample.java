749634bdd5858eda4d6eecaacfdaf33520d62028
Yatish <Yatish@DESKTOP-IK14MLI>
2023-06-14 15:28:47 +0530

    elevan commit

---
+++ dayeleven/src/org/tnsif/customerexception/CustomerExceptionExample.java
@@ -0,0 +1,28 @@
+package org.tnsif.customerexception;
+import java.util.*;
+public class CustomerExceptionExample {
+public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		String id = s.nextLine();
+		String password = s.nextLine();
+		try 
+		{
+	
+			if(id.equals("patilya4321@gmail.com") && password.equals("123456"))
+				{
+					System.out.println("Credentials Matched");
+				}
+			else
+				{
+					throw new LoginCredential("Invalid Credentials");
+				}
+		}
+			catch(LoginCredential e)
+		{
+			System.out.println("Exception handled " + e);
+		}
+
+	}
+
+}
---
+++ dayeleven/src/org/tnsif/customerexception/LoginCredential.java
@@ -0,0 +1,17 @@
+package org.tnsif.customerexception;
+
+public class LoginCredential extends Exception{
+	private String str1;
+
+	public LoginCredential(String str1) {
+		super();
+		this.str1 = str1;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginCredential [str1=" + str1 + "]";
+	}
+	
+
+}
