93c1e042d109cc19c10913a7d321a519db156997
Yatish <Yatish@DESKTOP-IK14MLI>
2023-06-16 14:05:43 +0530

    first Commit

---
+++ MoneyMoneyBank/src/org/tnsif/clent/MoneyMoneyBankClient.java
@@ -0,0 +1,34 @@
+package org.tnsif.clent;
+
+import org.tnsif.application.MMBankFactory;
+import org.tnsif.application.MMCurrentAcc;
+import org.tnsif.application.MMSavingAcc;
+import org.tnsif.framework.BankFactory;
+import org.tnsif.framework.CurrentAcc;
+import org.tnsif.framework.SavingAcc;
+
+public class MoneyMoneyBankClient {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		BankFactory b = new MMBankFactory();
+		SavingAcc s = new MMSavingAcc(4445, "Yatish Patil", 2778, true);
+		CurrentAcc c = new MMCurrentAcc(9889, "prathamesh Patil", 5000, 4500);
+		
+		System.out.println(s);
+		System.out.println(c);
+		
+		System.out.println();
+		
+		System.out.println("Saving Account");
+		
+		s.withdraw(200);
+		
+		
+		System.out.println("\nCurrent Account");
+		c.withdraw(400);
+		
+		
+	}
+
+}
