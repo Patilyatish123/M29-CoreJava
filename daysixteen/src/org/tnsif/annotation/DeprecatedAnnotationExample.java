458011111d85172b131ea1bbf0ece506d9de54d3
Yatish <Yatish@DESKTOP-IK14MLI>
2023-06-19 20:35:46 +0530

    first commit

---
+++ daysixteen/src/org/tnsif/annotation/DeprecatedAnnotationExample.java
@@ -0,0 +1,18 @@
+package org.tnsif.annotation;
+
+public class DeprecatedAnnotationExample {
+
+	
+	@Deprecated
+	public void print()
+	{
+		System.out.println("Welcome to Java Annotations");
+	}
+	
+	public static void main(String[] args)
+	{
+		DeprecatedAnnotationExample d = new DeprecatedAnnotationExample();
+		d.print();
+	}
+
+}
