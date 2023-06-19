5250d208ee8d94ac71c76d5fce66e5fcdac0b2b5
Yatish <Yatish@DESKTOP-IK14MLI>
2023-06-19 20:28:32 +0530

    first commit

---
+++ dayFifteen/src/org/tnsif/junit/FirstJunitTestCase.java
@@ -0,0 +1,19 @@
+package org.tnsif.junit;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+class FirstJunitTestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	@DisplayName("SimpleTesting")
+	void display()
+	{
+		System.out.println("First Junit5 Test case");
+	}
+
+}
